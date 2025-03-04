ce6cf2a4e9ce18219532561d826f04915d0668b4
Mr_Dzawara <shepherdnigel47@gmail.com>
2025-03-04 14:35:17 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Mr_Dzawara',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Mr_Dzawara",
+  botOwner: 'Mr_Dzawara', // Your name
+  ownerNumber: '263781974365', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
